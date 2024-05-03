7d73193fb941c97741ae1f256a726a27dc15a400
Rowan <volkwyn.rowan@gmail.com>
2024-05-03 11:35:55 +1000

    implemented routing system to display different html files

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
-    // set header content type and send a response
+    // set header content type
     res.setHeader('Content-Type', 'text/html');
-    res.write('<h1>Hello World!<h1>');
-    res.write('<p>Hello World! (in a paragraph)<p>');
-    res.end();
+
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    // read an html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.write(data);
+            res.end()
+        }
+    })
 });
 
 server.listen(3000, 'localhost', () => {
